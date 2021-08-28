7b5bbf5b6fa5f59a2d6f79ff12e889f592487b6d
Alex Lin <lib151886@gmail.com>
2021-08-28 23:38:22 +0800

    first commit

---
+++ vueJS/vue-lessons/src/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="submitData">
+        <input v-model="task">
+        <button> Submit </button>
+    </form>
+</template>
+
+
+<script>
+    export default {
+        data(){
+            return {
+                task: '',
+            };
+        },
+        methods: {
+            submitData(){
+                // this.$emit('自訂事件'[,.....]);
+                this.$emit('abc',this.task);
+                this.task = '';
+            },
+        },
+    }
+</script>
---
+++ vueJS/vue-lessons/src/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <li class="theItem"> {{xyz}} </li>
+</template>
+
+<script>
+    export default {
+        props: ['xyz'],
+    }
+</script>
+
+<style>
+    .theItem{
+        background: rgb(75, 211, 57);
+        font: 18px Calibri;
+        margin: 5px;
+        padding: 5px;
+    }
+    .theItem:hover{
+        background: rgb(212, 138, 63);
+        cursor: pointer;
+    }
+</style>
